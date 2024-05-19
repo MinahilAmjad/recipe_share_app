10675823d45e56a9aa567d3ed59c7ef703df2ca0
MinahilAmjad <153367588+MinahilAmjad@users.noreply.github.com>
2024-05-19 21:49:01 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Recipe Share App',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
